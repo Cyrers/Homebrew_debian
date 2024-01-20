a3666ba83cfc760d7934c33d3cbe18ee17e79667
hypo368 <114984095+hypo368@users.noreply.github.com>
2024-01-20 21:47:21 +0100

    ui

---
+++ Firefox/soft_user.js
@@ -0,0 +1,31 @@
+// Fichier qui regroupe les préférence du navigateur firefox pour le profil "soft"
+// Les préférences sont groupées par catégories
+
+/**************************************
+* Catégorie : HTML5 
+**************************************/
+
+// PREF : Désactive les notifications
+user_pref("dom.webnotifications.enabled", false); 
+
+// PREF : Désactive la géo-localiation
+user_pref("geo.enabled", false); 
+
+// PREF : Désactive la reconnaissance vocale
+user_pref("media.webspeech.recognition.enable", false); 
+
+// PREF : Désactive la synthèse vocale 
+user_pref("media.webspeech.synth.enabled", false); 
+
+// PREF : Désactive la reconnaissance faciale 
+user_pref("camera.controle.face_detection.enabled", false); 
+
+/*****************************************
+ * Catégorie : Misc
+ ****************************************/
+
+// Désactive les statistiques vidéo
+user_pref("media.video_stats.enabled", false); 
+
+
+
